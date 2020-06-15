2bbfe37db22a609c785190b82503d9fed355bb95
dvalverde <dvalverde>
2020-06-14 22:22:04 -0300

    Aula de Objeto

---
+++ Objeto/oo.js
@@ -0,0 +1,22 @@
+// CODIGO NÃO EXECUTAVEL
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() //Foco passou no objeto
+
+// Principios importantes
+// 1. abstração
+// 2. encapsulamento
+// 3. herança
+// 4. polimorfismo
